41670ca3bfde46c198992d117e5613c388627b5b
deepanshujoshi28 <deepanshujoshi2808@gmail.com>
2023-11-25 11:38:30 +0530

    ok

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Hello! world</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi quisquam molestiae libero. Ea incidunt, nihil nobis fuga esse culpa reiciendis, aut asperiores ipsa dolore, consequuntur perferendis? Quibusdam voluptatem quod quas blanditiis minima, neque delectus esse maxime, consequatur doloremque quae rerum dolorem dolore veritatis? Pariatur minus quia repellat doloribus, officiis ullam minima animi necessitatibus beatae eius eligendi ducimus possimus harum eos odit, in veritatis! Minus iure provident dolorem ipsum error alias sint magnam dignissimos at? Rem, quia! Unde ducimus vitae sapiente. Tempora quo corporis veritatis libero porro, tenetur assumenda quaerat maiores quidem expedita commodi ratione aspernatur eaque unde placeat atque. Voluptate.</p>
+        </div>
+    )
+}
+
+export default Home
